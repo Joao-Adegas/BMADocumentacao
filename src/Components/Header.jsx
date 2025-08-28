31a4d8f57bc8cd693d2f952c883b0b44b173a4b9
Joao-Adegas <pedrojoao6378@gmail.com>
2025-08-28 13:22:40 -0300

    Finalizado

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,15 +2,17 @@ import styles from "./Header.module.css";
 import { useLanguage } from "./LanguageText";
 
 export function Header() {
-  const { text, setLang } = useLanguage();
+  const { text, setLang, lang } = useLanguage();
 
   return (
     <header className={styles.header}>
       <h1>{text("title")}</h1>
       <p>{text("subtitle")}</p>
 
-      <button onClick={() => setLang("pt")}>🇧🇷 PT</button>
-      <button onClick={() => setLang("en")}>🇺🇸 EN</button>
+      <div className={styles.botaoTraducao}>
+        <button className={`${styles.botaoPortugues} ${lang === "pt" ? styles.ativo : ""}`} onClick={() => setLang("pt")}>PT 🇧🇷</button>
+        <button className={`${styles.botaoIngles} ${lang === "en" ? styles.ativo : ""}`} onClick={() => setLang("en")}>EN 🇺🇸</button>
+      </div>
     </header>
   );
 }
